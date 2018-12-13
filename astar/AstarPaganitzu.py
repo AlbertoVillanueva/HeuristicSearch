577ba61b86d39a40af24f69403a568c01da629d2
AlbertoVillanueva <32910015+AlbertoVillanueva@users.noreply.github.com>
2018-12-13 20:22:38 +0100

    Fixed esSitioPeligroso

--- astar/AstarPaganitzu.py
+++ astar/AstarPaganitzu.py
@@ -20,7 +20,11 @@ class node():
 		self.estado = estado
 		self.f = self.g + self.h()
 	def h(self):
-		return self.estado.al != salida
+		coste = 0
+		if sum(self.estado.llaves) < len(self.estado.llaves):
+			llaveLejos = max((abs(LLAVES[k][0]-self.estado.al[0])+abs(LLAVES[k][1]-self.estado.al[1]),LLAVES[k]) for k in range(len(LLAVES)) if not self.estado.llaves[k])
+			return 2*(llaveLejos[0]+ abs(llaveLejos[1][0]-salida[0])+abs(llaveLejos[1][1]-salida[1]))
+		return 2*(abs(self.estado.al[0]-salida[0])+abs(self.estado.al[1]-salida[1]))
 	def estaEn(self,l):
 		for i in range(len(l)):
 			if self.estado.al == l[i].estado.al and self.estado.llaves == l[i].estado.llaves and sorted(self.estado.rocas) == sorted(l[i].estado.rocas):
@@ -52,6 +56,7 @@ def astar():
 		print(backtracking(N))	
 	else:
 		print("Fracaso")
+	
 def esFinal(N):
 	return N.estado.al == salida
 def genSucesores(nodo):
@@ -62,7 +67,7 @@ def genSucesores(nodo):
 		nuevaPos = (estado.al[0]+m[0],estado.al[1]+m[1])
 		if sum(estado.llaves) == len(LLAVES) and nuevaPos == salida:
 			sucesores.append(node(nodo,2, state(nuevaPos,estado.rocas[:],estado.llaves[:])))
-		elif not MUROS[nuevaPos[0]][nuevaPos[1]] and not esSitioPeligroso(nuevaPos):
+		elif not MUROS[nuevaPos[0]][nuevaPos[1]] and not esSitioPeligroso(nuevaPos,estado):
 			if nuevaPos in estado.rocas:
 				unaMasHaciaEsaDireccionPrimo = (nuevaPos[0]+m[0],nuevaPos[1]+m[1])
 				if not(unaMasHaciaEsaDireccionPrimo in estado.rocas or MUROS[unaMasHaciaEsaDireccionPrimo[0]][unaMasHaciaEsaDireccionPrimo[1]] or hayLlave(unaMasHaciaEsaDireccionPrimo,estado)):
@@ -109,15 +114,21 @@ def backtracking(N):
 	while nodo != None:
 		path.append(nodo.estado.al)
 		nodo = nodo.padre
-	return path[::-1]
-def esSitioPeligroso(pos):
+	path = path[::-1]
+	directions = {(0,1):"Right",(0,-1):"Left",(1,0):"Down",(-1,0):"Up"}
+	path_directions = []
+	for i in range(len(path)-1):
+		diff = (-path[i][0]+path[i+1][0],-path[i][1]+path[i+1][1])
+		path_directions.append(directions[diff])
+	return path_directions
+def esSitioPeligroso(pos,estado):
 	i = pos[1]-1
-	while not MUROS[pos[0]][i]:
+	while not MUROS[pos[0]][i] and (pos[0],i) not in estado.rocas:
 		i-=1
 	if (pos[0],i) in SERPIENTES:
 		return True
 	i = pos[1]+1
-	while not MUROS[pos[0]][i]:
+	while not MUROS[pos[0]][i] and (pos[0],i) not in estado.rocas:
 		i+=1
 	if (pos[0],i) in SERPIENTES:
 		return True
@@ -134,12 +145,13 @@ SERPIENTES = []
 salida = (None,None)
 rocas = []
 al = (None,None)
-'''
-f = open('lab_astar/lab1.map','r')
+
+f = open('lab_astar/lab4.map','r')
 archivo = f.read()
 f.close()
-'''
-archivo='%%%%%%%%%%\n%    %%% %\n% O   K  E\n%A      %%\n%%%%%%%%%%\n'
+
+#archivo='%%%%%%%%%%\n%    %%% %\n% O   K  E\n%A      %%\n%%%%%%%%%%\n'
+#archivo='%%%%%%%%%%%%\n%A  K%%%   %\n% O        %\n%S      %%%%\n%%%%%E%%%%%%'
 archivo = archivo.split('\n')
 for i in range(len(archivo)):
 	fila = []
